2aa3f740c6fe12ede631378e80110b9f3beb1ffb
Akley Alves <akleyalvescr@gmail.com>
2023-12-05 15:18:10 -0300

    Create Personagens.java

---
+++ src/main/java/com/ipog/bg/model/Personagens.java
@@ -0,0 +1,53 @@
+package com.ipog.bg.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="personagens")
+
+public class Personagens {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long ID;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	// Construtor padrão, sem atributos
+	public Personagens() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	// Construtor com todos os campos
+	public Personagens(Long iD, String nome) {
+		super();
+		ID = iD;
+		this.nome = nome;
+	}
+	
+	
+	// Todos os getters e setters
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long iD) {
+		ID = iD;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
